68831d5a50467abb2f4875a575c25304a36a4bac
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2022-10-19 14:29:19 -0700

    added h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +16,5 @@ int main (void)
 	_putchar('r');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
